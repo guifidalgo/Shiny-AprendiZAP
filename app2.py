24c1438afece4ee863db0e64902dfd680c235728
guifidalgo <guifidalgo@gmail.com>
2025-09-02 18:34:17 -0300

    Changes

--- app2.py
+++ app2.py
@@ -13,25 +13,26 @@ color_map = {
     4: "#F4B93C",  # Amarelo dourado
     5: "#E85C4A",  # Vermelho coral
 }
-cor_completude_map = {
-    "0-20%":  color_map[5],
-    "20-40%": color_map[4],
-    "40-60%": color_map[3],
-    "60-80%": color_map[2],
-    "80-100%": color_map[1]   # equivalente ao else da função
-}
 
-teachers = pd.read_parquet("data/teachers.parquet")
-faixas_completude = teachers['faixa_completude'].unique().tolist()
-faixas_completude.sort()
 
-interactions = pd.read_parquet("data/teachers_interactions.parquet")
-interactions['data_inicio'] = pd.to_datetime(interactions['data_inicio'])
+teachers = pd.read_parquet("data/teachers_entries.parquet")
+teachers['semana_entrada'] = pd.to_datetime(teachers['semana_entrada']).dt.tz_localize(None)
+teachers['usuario_valido'] = teachers['selectedstages'].notna()
+r_score = teachers['R_score'].unique().tolist()
+r_score.sort()
+f_score = teachers['F_score'].unique().tolist()
+f_score.sort()
+m_score = teachers['M_score'].unique().tolist()
+m_score.sort()
+rfm_score = teachers['RFM_Score'].unique().tolist()
+rfm_score.sort()
 
 
 icon_teacher = '<img src="https://github.com/guifidalgo/Shiny-AprendiZAP/blob/master/assets/teacher_white.png?raw=true" alt="Professor" width="50" height="50">'
 icon_student = '<img src="https://github.com/guifidalgo/Shiny-AprendiZAP/blob/master/assets/student_white.png?raw=true" alt="Aluno" width="50" height="50">'
 icon_interaction = '<img src="https://github.com/guifidalgo/Shiny-AprendiZAP/blob/master/assets/interaction_white.png?raw=true" alt="Interação" width="50" height="50">'
+icon_speedometer = '<img src="https://github.com/guifidalgo/Shiny-AprendiZAP/blob/master/assets/speedometer_white.png?raw=true" alt="Velocímetro" width="50" height="50">'
+icon_stopwatch = '<img src="https://github.com/guifidalgo/Shiny-AprendiZAP/blob/master/assets/stopwatch_white.png?raw=true" alt="Cronômetro" width="50" height="50">'
 
 
 app_ui = ui.page_sidebar(
@@ -42,49 +43,72 @@ app_ui = ui.page_sidebar(
             'Apenas Usuários Válidos',
             value=False
             ),
-        ui.input_selectize(
-            'select_completude',
-            'Selecione as faixas de completude do cadastro',
-            choices=faixas_completude,
-            multiple=True,
-            selected=faixas_completude,  # Seleciona todas as faixas por padrão
+        ui.input_slider(
+            "slider_rfm_score",
+            label="Score RFM",
+            min=rfm_score[0],
+            max=rfm_score[-1],
+            value=(rfm_score[0], rfm_score[-1])
         ),
+        # ui.input_selectize(
+        #     'select_r_score',
+        #     'Recência',
+        #     choices=r_score,
+        #     multiple=True,
+        #     selected=r_score,  # Seleciona todas as faixas por padrão
+        # ),
+        # ui.input_selectize(
+        #     'select_f_score',
+        #     'Frequência',
+        #     choices=f_score,
+        #     multiple=True,
+        #     selected=f_score,  # Seleciona todas as faixas por padrão
+        # ),
+        # ui.input_selectize(
+        #     'select_m_score',
+        #     'Monetização (Tempo Gasto)',
+        #     choices=m_score,
+        #     multiple=True,
+        #     selected=m_score,  # Seleciona todas as faixas por padrão
+        # ),
         ui.input_slider(
             "slider_date",
             label="Data de Cadastro:",
-            min=teachers['data_entrada'].min(),
-            max=teachers['data_entrada'].max(),
-            value=(teachers['data_entrada'].min(), teachers['data_entrada'].max()),
-            time_format="%m/%Y",
-        ),
-        ui.input_slider(
-            "slider_interacoes",
-            label="Data de Interações:",
-            min=interactions['data_inicio'].min(),
-            max=interactions['data_inicio'].max(),
-            value=(interactions['data_inicio'].min(), interactions['data_inicio'].max()),
+            min=teachers['semana_entrada'].min(),
+            max=teachers['semana_entrada'].max(),
+            value=(teachers['semana_entrada'].min(), teachers['semana_entrada'].max()),
             time_format="%m/%Y",
         ),
         bg="#f8f8f8"
     ),
     ui.layout_columns(
         ui.value_box(
-            "Professores",
+            "Cadastros",
             ui.output_text("qtd_professores"),
+            "Professores",
             showcase=ui.HTML(icon_teacher),
             theme=ui.value_box_theme('aprendizap', bg=color_map[0], fg="white")
         ),
         ui.value_box(
-            "Média de Alunos por Professor",
-            ui.output_text("media_alunos_por_professor"),
-            showcase=ui.HTML(icon_student),
+            "Score RFM Médio",
+            ui.output_text("media_score_rfm"),
+            "Pontos",
+            showcase=ui.HTML(icon_speedometer),
             theme=ui.value_box_theme('aprendizap3', bg=color_map[2], fg="white")
         ),
         ui.value_box(
-            "Interações de Professores",
-            ui.output_text("qtd_interacoes"),
+            "Frequência Média de Acessos",
+            ui.output_text("media_acessos"),
+            "Acessos",
             showcase=ui.HTML(icon_interaction),
             theme=ui.value_box_theme('aprendizap2', bg=color_map[1], fg="white")
+        ),
+        ui.value_box(
+            "Tempo Médio Gasto",
+            ui.output_text("media_duracao_acessos"),
+            "Minutos",
+            showcase=ui.HTML(icon_stopwatch),
+            theme=ui.value_box_theme('aprendizap2', bg=color_map[3], fg="white")
         ),
         ui.card(
             ui.card_header("Cadastro de Professores"),
@@ -104,19 +128,13 @@ app_ui = ui.page_sidebar(
                 ui.output_plot("plot_utm_origem")
             )
         ),
-        # ui.card(
-        #     ui.card_header("Interações por Data de Entrada"),
-        #     ui.card_body(
-        #         ui.output_plot("plot_interacoes_data_entrada")
-        #     )
-        # ),
         ui.card(
-            ui.card_header("Interações ao Longo do Tempo"),
+            ui.card_header("Matriz RFM - Recência, Frequência e Tempo Gasto na Plataforma"),
             ui.card_body(
-                ui.output_plot("plot_interacoes_data_inicio")
+                ui.output_plot("plot_matriz_rfm")
             )
         ),
-        col_widths=[3, 3, 3, 12, 6, 6, 12]
+        col_widths=[3, 3, 3, 3, 12, 6, 6, 12]
     ),
     title="AprendiZAP - Grupo 01",
 )
@@ -126,30 +144,19 @@ app_ui = ui.page_sidebar(
 def server(input, output, session):
     @reactive.calc
     def teachers_filtrado():
-        start_date = pd.to_datetime(input.slider_date()[0]).tz_localize(TIMEZONE)
-        end_date = pd.to_datetime(input.slider_date()[1]).tz_localize(TIMEZONE)
+        start_date = pd.to_datetime(input.slider_date()[0]).tz_localize(None)
+        end_date = pd.to_datetime(input.slider_date()[1]).tz_localize(None)
+        start_rfm = input.slider_rfm_score()[0]
+        end_rfm = input.slider_rfm_score()[1]
         if input.switch_valid():
             df = teachers[teachers['usuario_valido'] == True]
         else:
             df = teachers
         return df[
-            (df['data_entrada'] >= start_date) &
-            (df['data_entrada'] <= end_date) &
-            (df['faixa_completude'].isin(input.select_completude()))
-            ]
-    
-    @reactive.calc
-    def interactions_filtrado():
-        start_date = pd.to_datetime(input.slider_interacoes()[0]).tz_localize(TIMEZONE)
-        end_date = pd.to_datetime(input.slider_interacoes()[1]).tz_localize(TIMEZONE)
-        if input.switch_valid():
-            df = interactions[interactions['usuario_valido'] == True]
-        else:
-            df = interactions
-        return df[
-            (df['data_inicio'] >= start_date) &
-            (df['data_inicio'] <= end_date) &
-            (df['faixa_completude'].isin(input.select_completude()))
+            (df['semana_entrada'] >= start_date) &
+            (df['semana_entrada'] <= end_date) &
+            (df['RFM_Score'] >= start_rfm) &
+            (df['RFM_Score'] <= end_rfm)
         ]
 
 
@@ -158,22 +165,27 @@ def server(input, output, session):
         return f"{teachers_filtrado().shape[0]:,d}"
     
     @render.text
-    def qtd_interacoes():
-        df = interactions_filtrado()
-        return f"{df['unique_id'].count():,.0f}"
+    def media_score_rfm():
+        df = teachers_filtrado()
+        return f"{df['RFM_Score'].mean():,.2f}"
+
+    @render.text
+    def media_acessos():
+        df = teachers_filtrado()
+        return f"{df['Frequency'].mean():,.2f}"
     
     @render.text
-    def media_alunos_por_professor():
+    def media_duracao_acessos():
         df = teachers_filtrado()
-        return f"{df['total_alunos'].mean():,.2f}"
+        return f"{df['Duration'].mean():,.2f}"
 
     @render.plot
     def plot_cadastros():
         df = teachers_filtrado()
-        semanais = df.groupby(['data_semana_entrada']).size().reset_index(name='cadastros_count')
-        semanais['data_semana_entrada'] = pd.to_datetime(semanais['data_semana_entrada'])
+        semanais = df.groupby(['semana_entrada']).size().reset_index(name='cadastros_count')
+        semanais['semana_entrada'] = pd.to_datetime(semanais['semana_entrada'])
         plot = (
-            p9.ggplot(semanais, p9.aes(x='data_semana_entrada', y='cadastros_count')) +
+            p9.ggplot(semanais, p9.aes(x='semana_entrada', y='cadastros_count')) +
             p9.geom_area(fill=color_map[0], alpha=0.5) +
             p9.geom_line(color=color_map[0]) +
             p9.labs(
@@ -234,40 +246,25 @@ def server(input, output, session):
         return plot
     
     @render.plot
-    def plot_interacoes_data_entrada():
+    def plot_matriz_rfm():
         df = teachers_filtrado()
-        interacoes_data = df.groupby(['data_semana_entrada', 'faixa_completude']).agg({'interactions_count': 'sum'}).reset_index()
-        interacoes_data['data_semana_entrada'] = pd.to_datetime(interacoes_data['data_semana_entrada'])
+        heatmap = df.groupby(['R_score', 'F_score']).agg({'Duration': 'mean'}).reset_index()
+        heatmap['p_cut'] = pd.qcut(heatmap['Duration'], 2, labels=("low", "high"))
+        heatmap['Duration'] = heatmap['Duration'].round(1)
         plot = (
-            p9.ggplot(interacoes_data, p9.aes(x='data_semana_entrada', y='interactions_count', fill='faixa_completude')) +
-            p9.geom_area(position='stack', alpha=0.5) +
-            p9.scale_fill_manual(values=cor_completude_map) +
+            p9.ggplot(heatmap, p9.aes(x='R_score', y='F_score', fill='Duration')) +
+            p9.geom_tile(p9.aes(width=0.95, height=0.95)) +
+            p9.geom_text(p9.aes(label='Duration', color='p_cut'), size=9, show_legend=False) +
+            p9.scale_color_manual(['white', 'black']) +
+            p9.scale_fill_cmap(cmap_name='viridis', trans="log10") +
+            # p9.scale_x_discrete(limits=heatmap['R_score'].unique()[::-1]) +
+            # p9.scale_y_discrete(limits=heatmap['F_score'].unique()[::-1]) +
             p9.labs(
-                x='Data de Entrada',
-                y='Número de Interações',
-                fill='Faixa de Completude'
-            ) +
-            p9.theme_minimal()
-        )
+        x="Recência",
+        y="Frequência",
+        fill="Tempo"
+    )
+)
         return plot
-    
-    @render.plot
-    def plot_interacoes_data_inicio():
-        df = interactions_filtrado()
-        semanais = df.groupby(['data_semana_inicio', 'faixa_completude']).agg({'unique_id': 'count'}).reset_index()
-        semanais['data_semana_inicio'] = pd.to_datetime(semanais['data_semana_inicio'])
-        plot = (
-            p9.ggplot(semanais, p9.aes(x='data_semana_inicio', y='unique_id', color='faixa_completude')) +
-            p9.geom_line() +
-            p9.scale_color_manual(values=cor_completude_map) +
-            p9.labs(
-                x='Data de Interação',
-                y='Número de Interações',
-                color='Faixa de Completude'
-            ) +
-            p9.theme_minimal()
-        )
-        return plot
-
 
 app = App(app_ui, server)
